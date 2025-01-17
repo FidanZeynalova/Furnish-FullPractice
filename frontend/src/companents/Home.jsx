6537a70dc57dd9df278cc569556c55e040abc22e
ulvieyvazov <eyvazovulvi010101@gmail.com>
2025-01-17 18:26:26 +0400

    Task Done

--- frontend/src/companents/Home.jsx
+++ frontend/src/companents/Home.jsx
@@ -4,16 +4,16 @@ function Home() {
     return (
         <div className='Home'>
             <div className="container">
-                <div className="left">
-                    <div className="content">
+                <div className="left" style={{height:"800px"}}>
+                    <di v className="content">
                         <p>Best Design of</p>
                         <h1>Furniture Collections</h1>
                         <span>A small river named Duden flows by their place and supplies it with the necessary regelialia.</span>
                         <button>Discover</button>
-                    </div>
+                    </di>
                 </div>
                 <div className="right">
-                    <img src="https://preview.colorlib.com/theme/furnish/images/bg_2.jpg.webp" alt="" />
+                    <img src="https://preview.colorlib.com/theme/furnish/images/bg_2.jpg.webp" alt=""  style={{height:"800px"}}/>
                 </div>
             </div>
         </div>
